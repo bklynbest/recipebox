714b348a093540ffb7cc0a71ecae69492cb4f485
Jonathan Momperousse <bklynbest718@gmail.com>
2018-07-01 20:28:42 +0000

    added image colum to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701165041) do
+ActiveRecord::Schema.define(version: 20180701202818) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180701165041) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
 end
